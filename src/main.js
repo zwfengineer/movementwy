58026838aa1a0b8b6a97d32785ef89c956a4de6a
hhy <1654620778@qq.com>
2022-11-26 23:50:14 +0800

    feat:bying静态

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import element from "element-plus"
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import "element-plus/dist/index.css"
+import './view/buying/css/reset.css'
 
 import './style.css'
 import App from './App.vue'
