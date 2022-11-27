59ea8e836d8548d5d4906361fa2ab4d0bd3e2054
zwf <1358859314@qq.com>
2022-11-27 17:37:27 +0800

    为tabbar 添加了图标，搜索结果页动态数据动态展示改造

--- src/components/navbar/index.vue
+++ src/components/navbar/index.vue
@@ -1,29 +1,39 @@
 <template>
-  <div v-show="route.meta.showbar">
-    <el-tabs class="demo-tabs" tap-position="bottom" @tab-click="handleClick">
+  <div v-show="route.meta.showbar" class="navbar">
+    <el-tabs class="demo-tabs" tap-position="bottom" type="card" stretch @tab-click="handleClick">
       <el-tab-pane>
         <template #label>
-          <RouterLink to="/home"> 主页 </RouterLink>
+          <RouterLink to="/home">
+            <el-icon :size="iconsize"><House /></el-icon>
+          </RouterLink>
         </template>
       </el-tab-pane>
       <el-tab-pane>
         <template #label>
-          <RouterLink to="/category"> 分类 </RouterLink>
+          <RouterLink to="/category">
+            <el-icon :size="iconsize"><Grid /></el-icon>
+          </RouterLink>
         </template>
       </el-tab-pane>
       <el-tab-pane>
         <template #label>
-          <RouterLink to="/buying"> 值得买 </RouterLink>
+          <RouterLink to="/buying">
+            <el-icon :size="iconsize"><Reading /></el-icon>
+          </RouterLink>
         </template>
       </el-tab-pane>
       <el-tab-pane>
         <template #label>
-          <RouterLink to="/shopcart"> 购物车 </RouterLink>
+          <RouterLink to="/shopcart">
+            <el-icon :size="iconsize"><ShoppingCart /></el-icon>
+          </RouterLink>
         </template>
       </el-tab-pane>
       <el-tab-pane>
         <template #label>
-          <RouterLink to="/personal"> 用户 </RouterLink>
+          <RouterLink to="/personal">
+            <el-icon :size="iconsize"><User /></el-icon>
+          </RouterLink>
         </template>
       </el-tab-pane>
     </el-tabs>
@@ -31,20 +41,33 @@
 </template>
 
 <script setup>
+import {
+  House,
+  Grid,
+  User,
+  ShoppingCart,
+  Reading,
+} from "@element-plus/icons-vue";
 import { useRoute } from "vue-router";
+import { ref } from "vue";
 const route = useRoute();
+const iconsize = ref("25px");
 </script>
 <script>
-export default{
-    name:"Navbar"
-}
+export default {
+  name: "Navbar",
+};
 </script>
 
 <style scoped>
 .el-tabs {
-  position: fixed;
-  bottom: 0px;
-  left: 0px;
-  height: 2rem;
+  display: flex;
+  justify-content: space-evenly;
+}
+.navbar{
+  position:fixed;
+  bottom: 25px;
+  height: 10px;
+  width: 100%;
 }
 </style>
