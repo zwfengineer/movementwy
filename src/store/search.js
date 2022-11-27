63f4e781bb5b62c12bdc333b7158923adbf6f7e6
zwf <1358859314@qq.com>
2022-11-27 23:18:06 +0800

    完善动态展示搜索结果

--- src/store/search.js
+++ src/store/search.js
@@ -1,13 +1,22 @@
-import {defineStore} from "pinia"
-
+import { defineStore } from "pinia";
+import request from "@/util/request.js";
 const useSearchStore = defineStore({
-    id:"search",
-    state:{
+  id: "search",
+  state: () => ({
+    directlyList:null
+  }),
+  actions: {
+    async search(keyword) {
+      let result = await request.get("/xhr/search/search.json", {
+        params: {
+          size: 40,
+          keyword: keyword,
+        },
 
+      });
+      this.directlyList = result.data.directlyList
     },
-    actions:{
+  }
+});
 
-    }
-})
-
-export default useSearchStore;
+export { useSearchStore };
