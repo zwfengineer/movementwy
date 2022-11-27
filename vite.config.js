31ac5cf1f70a9275f0186d1f3c28aec5706141e4
zwf <1358859314@qq.com>
2022-11-27 11:28:45 +0800

    消除冲突

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,12 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import {resolve} from "path"
 import vue from '@vitejs/plugin-vue'
 
-=======
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import {resolve} from "path"
->>>>>>> ed98f4e (hotfix: searchbox)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-<<<<<<< HEAD
       '@': resolve(__dirname, "src"),
     },
     extensions: [".ts", ".vue", ".js", ".jsx", ".tsx"], // 导入时想要省略的扩展名列表。
@@ -22,16 +15,6 @@ export default defineConfig({
     proxy:{
       "/api":{
         target:"http://sph-h5-api.atguigu.cn"
-=======
-      "@": resolve(__dirname, "src"),
-    },
-    extensions: [".ts", ".vue", ".js", ".jsx", ".tsx"], // 导入时想要省略的扩展名列表。
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://sph-h5-api.atguigu.cn",
->>>>>>> ed98f4e (hotfix: searchbox)
       },
       "/netEase": {
         target: "https://m.you.163.com",
