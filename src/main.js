01af00e297c90f5e9139e2087e441bd947c719d7
hhy <1654620778@qq.com>
2022-11-28 22:34:52 +0800

    整理代码

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import element from "element-plus"
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import "element-plus/dist/index.css"
-import './view/buying/css/reset.css'
 import 'amfe-flexible'
 
 import './style.css'
