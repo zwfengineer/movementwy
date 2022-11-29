bc3823b88e337964b6ff35b4574fe2a4587659d2
zwf <1358859314@qq.com>
2022-11-29 09:15:40 +0800

    fix：fontsize

--- src/main.js
+++ src/main.js
@@ -1,28 +1,25 @@
 import { createApp } from "vue";
 import element from "element-plus";
 import zhCn from "element-plus/es/locale/lang/zh-cn";
-import "element-plus/dist/index.css";
-import "./view/buying/css/reset.css";
-import "amfe-flexible";
-import vant from 'vant'
-import 'vant/lib/index.css';
-import "./style.css";
+
+import vant from "vant";
 import App from "./App.vue";
 import Navbar from "@/components/navbar/index.vue";
-import './assets/css/reset.css';
-import './assets/css/font/iconfont.css'
-import 'amfe-flexible'
+import "vant/lib/index.css";
+import "./style.css";
+import "element-plus/dist/index.css";
+import "./assets/css/reset.css";
+import "./assets/css/font/iconfont.css";
+import "amfe-flexible";
 
-import router from "./routers"
-import pinia from './store'
-// import './assets/iconfont/iconfont.styl'
-
+import router from "./routers";
+import pinia from "./store";
 
 const app = createApp(App);
 app
   .use(router)
   .component("Navbar", Navbar)
   .use(pinia)
-.use(vant)
+  .use(vant)
   .use(element, { local: zhCn })
   .mount("#app");
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
-import routes from "./routers"
-const router= createRouter({
-    history:createWebHistory(),
-    routes,
-})  
+import routes from "./routers";
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
 
+const resetfontsize = (fontsize) => {
+  const html = document.querySelector("html");
+  html.style.fontSize = fontsize;
+};
 
+router.afterEach((to, from) => {
+  if (to.name == "shopcart") {
+    resetfontsize("23.45px");
+  }else{
+    resetfontsize("37.5px")
+  }
+});
 
-export default router
+export default router;
